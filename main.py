7af95df589c6ce790325bdd18243c428a9701111
timofey Labzeev <timosha9911@gmail.com>
2023-06-26 14:10:55 +0300

    add work without enter PUT

--- main.py
+++ main.py
@@ -201,11 +201,20 @@ def get_all_credits():
 @app.route("/api/v1/credits", methods=["PUT"])
 def create_credit():
     # Получаем данные из запроса в формате JSON
-    client_id = request.args.get('client_id')
-    percent = request.args.get('percent')
-    sum = request.args.get('sum')
-    term = request.args.get('term')
-    periods = request.args.get('periods')
+    
+    if request.json:
+        data = request.json
+        client_id = data["client_id"]
+        percent = data["percent"]
+        sum = data["sum"]
+        term = data["term"]
+        periods = data["periods"]
+    else:
+        client_id = request.args.get('client_id')
+        percent = request.args.get('percent')
+        sum = request.args.get('sum')
+        term = request.args.get('term')
+        periods = request.args.get('periods')
 
     with open("credits_deposits.yaml", "r") as f:
         file_data = yaml.safe_load(f)
