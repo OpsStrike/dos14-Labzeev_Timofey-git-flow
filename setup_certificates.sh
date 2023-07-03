67c0e21da3b3c95bea1893009b641bb045bfd387
timofey Labzeev <timosha9911@gmail.com>
2023-07-03 13:15:23 +0300

    rework all

--- setup_certificates.sh
+++ setup_certificates.sh
@@ -1,22 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
-# Получаем сертификаты с помощью Certbot
-certbot certonly --webroot \
+existing_cert=$(certbot certificates --domains tla.bank.smodata.net --cert-name tla.bank.smodata.net --format json)
+
+cert_status=$(echo "$existing_cert" | jq -r '.[] | .status')
+
+if [ "$cert_status" == "OK" ]; then
+  echo "Существует действующий и валидный сертификат. Пропуск выписывания нового сертификата."
+else
+  echo "Выпуск нового сертификата..."
+  certbot certonly --webroot \
     --webroot-path=/var/www/html \
     --email your-timosha9911@gmail.com \
     --agree-tos \
     --no-eff-email \
-    --force-renewal \
     -d tla.bank.smodata.net
 
-# Копируем сертификаты в контейнер с Nginx
-echo "Копирование сертификатов в контейнер с Nginx..."
-docker cp /etc/letsencrypt/live/tla.bank.smodata.net/fullchain.pem nginx:/etc/nginx/certs/fullchain.pem
-docker cp /etc/letsencrypt/live/tla.bank.smodata.net/privkey.pem nginx:/etc/nginx/certs/privkey.pem
-
-# Перезапуск контейнера Nginx с использованием Docker Remote API
-echo "Перезапуск контейнера с Nginx..."
-DOCKER_HOST=unix:///var/run/docker.sock docker restart nginx
+fi
 
 echo "Настройка сертификатов завершена"
+
 
