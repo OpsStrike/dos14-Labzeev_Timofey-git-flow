f5dd11adab404df60966cd5130d54c44a4151026
timofey Labzeev <timosha9911@gmail.com>
2023-07-04 16:56:43 +0300

    bacnk to 2 containers

---
+++ setup_certificates.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+existing_cert=$(certbot certificates --domains tla.bank.smodata.net --cert-name tla.bank.smodata.net --format json)
+
+cert_status=$(echo "$existing_cert" | jq -r '.[] | .status')
+
+if [ "$cert_status" == "OK" ]; then
+  echo "Существует действующий и валидный сертификат. Пропуск выписывания нового сертификата."
+else
+  echo "Выпуск нового сертификата..."
+  certbot certonly --nginx \
+    --non-interactive \
+    --email your-timosha9911@gmail.com \
+    --agree-tos \
+    --no-eff-email \
+    -d tla.bank.smodata.net
+fi
+echo "Настройка сертификатов завершена"
