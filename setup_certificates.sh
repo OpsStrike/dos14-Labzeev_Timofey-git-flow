b12edee1b1f3e162d8fd7070d90f7fb6615c682a
timofey Labzeev <timosha9911@gmail.com>
2023-07-13 13:11:41 +0300

    add exit codes to certbot

--- setup_certificates.sh
+++ setup_certificates.sh
@@ -1,17 +1,27 @@
 #!/bin/sh
-
+error_log="/var/log/letsencrypt/letsencrypt.log"
 cert_path="/etc/letsencrypt/live/tla.bank.smodata.net/fullchain.pem"
 if [ -f "$cert_path" ]; then
-  echo "Сертификат есть"
-else
-  echo "Выпуск нового сертификата..."
-  certbot certonly --standalone --preferred-challenges http \
-    --http-01-port 80 \
-    --non-interactive \
-    --email timosha9911@gmail.com \
-    --agree-tos \
-    --no-eff-email \
-    -d tla2.bank.smodata.net
+  echo "Сертификаты уже существуют"
+  exit 0
 fi
+
+echo "Выпуск нового сертификата..."
+
+if certbot certonly --standalone --preferred-challenges http \
+  --http-01-port 80 \
+  --non-interactive \
+  --email timosha9911@gmail.com \
+  --agree-tos \
+  --no-eff-email \
+  -d tla.bank.smodata.net; then
+  echo "Сертификаты успешно созданы"
+  exit 10
+else
+  echo "Произошла ошибка при выпуске сертификатов"
+  exit 1
+fi
+
 echo "Настройка сертификатов завершена"
+
 
