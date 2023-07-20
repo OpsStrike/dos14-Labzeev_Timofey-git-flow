649109acd949271f876b2cfd43460e60e00d2fd0
timofey Labzeev <timosha9911@gmail.com>
2023-07-20 17:05:08 +0300

    + create tables credits and deposits

--- check_and_create_DB.py
+++ check_and_create_DB.py
@@ -39,11 +39,52 @@ def check_and_create_database():
             print(f"Database '{database}' created successfully.")
             cursor.close()
             connection.close()
+            
+            database_created = True
+            
         except psycopg2.Error as e:
             error_message = f"{datetime.datetime.now()} - Error while creating database: {e}"
             with open("errors_DB.yml", "a") as error_file:
                 error_file.write(error_message + "\n")
                 
+    if database_created:
+        try:
+            connection = psycopg2.connect(
+                host=host,
+                port=port,
+                database=database,
+                user=username,
+                password=password
+            )
+            connection.autocommit = True
+            cursor = connection.cursor()
+            
+            cursor.execute("""
+                CREATE TABLE IF NOT EXISTS Credits (
+                    credit_id INTEGER PRIMARY KEY,
+                    client_id INTEGER,
+                    percent NUMERIC,
+                    sum NUMERIC,
+                    term INTEGER,
+                    periods INTEGER
+                );
+            """)
+            print("Table 'Credits' created successfully.")
+            
+            cursor.execute("""
+                CREATE TABLE IF NOT EXISTS Deposits (
+                    deposit_id INTEGER PRIMARY KEY,
+                    client_id INTEGER,
+                    amount NUMERIC,
+                    duration INTEGER
+                );
+            """)
+            print("Table 'Deposits' created successfully.")
+
+            cursor.close()
+            connection.close()          
+    
+     	                  
 if __name__ == "__main__":
     check_and_create_database()
     
