4bf5f8b8d3ce6f2b718bc27a4286ef488f44ba82
timofey Labzeev <timosha9911@gmail.com>
2023-07-20 16:33:32 +0300

    new script for check and  create DB

---
+++ check_and_create_DB.py
@@ -0,0 +1,50 @@
+import psycopg2
+import datetime
+
+with open("secrets_decrypted.yml", "r") as f:
+	password = f.read()
+
+host = "localhost"
+port = 5432
+database = "omegabank"
+username = "postgres"
+
+
+def check_and_create_database():
+    try:
+        connection = psycopg2.connect(
+            host=host,
+            port=port,
+            database=database,
+            user=username,
+            password=password
+        )
+
+        print(f"Connected to the database '{database}'.")
+        connection.close()
+        
+    except psycopg2.Error as i:
+        error_message = f"{datetime.datetime.now()} - Error: {e}"   
+    	with open("errors_DB.yml", "a") as error_file:
+        	error_file.write(error_message + "\n")
+        try:
+            connection = psycopg2.connect(
+                host=host,
+                port=port,
+                user=username,
+                password=password
+            )
+            cursor = connection.cursor()
+            cursor.execute(f"CREATE DATABASE {database};")
+            print(f"Database '{database}' created successfully.")
+            cursor.close()
+            connection.close()
+        except psycopg2.Error as e:
+            error_message = f"{datetime.datetime.now()} - Error while creating database: {e}"
+            with open("errors_DB.yml", "a") as error_file:
+                error_file.write(error_message + "\n")
+                
+if __name__ == "__main__":
+    check_and_create_database()
+    
+
