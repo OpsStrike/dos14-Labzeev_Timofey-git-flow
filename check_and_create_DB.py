22b237a0470790462513baf6f151f171f3b1bd43
timofey Labzeev <timosha9911@gmail.com>
2023-07-21 11:12:07 +0300

    fix viriables

--- check_and_create_DB.py
+++ check_and_create_DB.py
@@ -9,7 +9,6 @@ port = 5432
 database = "omegabank"
 username = "postgres"
 
-
 def check_and_create_database():
     try:
         connection = psycopg2.connect(
@@ -19,12 +18,10 @@ def check_and_create_database():
             user=username,
             password=password
         )
-
-        print(f"Connected to the database '{database}'.")
+        print(f"Подключено к базе данных '{database}'.")
         connection.close()
-
-    except psycopg2.Error as i:
-        error_message = f"{datetime.datetime.now()} - Error: {e}"
+    except psycopg2.OperationalError as e:
+        error_message = f"{datetime.datetime.now()} - Ошибка: {e}"
         with open("errors_DB.yml", "a") as error_file:
             error_file.write(error_message + "\n")
         try:
@@ -36,17 +33,16 @@ def check_and_create_database():
             )
             cursor = connection.cursor()
             cursor.execute(f"CREATE DATABASE {database};")
-            print(f"Database '{database}' created successfully.")
+            print(f"База данных '{database}' успешно создана.")
             cursor.close()
             connection.close()
 
             database_created = True
 
-        except psycopg2.Error as e:
-            error_message = f"{datetime.datetime.now()} - Error while creating database: {e}"
+        except psycopg2.ProgrammingError as e:
+            error_message = f"{datetime.datetime.now()} - Ошибка при создании базы данных: {e}"
             with open("errors_DB.yml", "a") as error_file:
                 error_file.write(error_message + "\n")
-                
     
     if database_created:
         try:
@@ -57,12 +53,11 @@ def check_and_create_database():
                 user=username,
                 password=password
             )
-            connection.autocommit = True
             cursor = connection.cursor()
 
             cursor.execute("""
                 CREATE TABLE IF NOT EXISTS Credits (
-                    credit_id INTEGER PRIMARY KEY,
+                    credit_id PRIMARY KEY,
                     client_id INTEGER,
                     percent NUMERIC,
                     sum NUMERIC,
@@ -70,24 +65,26 @@ def check_and_create_database():
                     periods INTEGER
                 );
             """)
-            print("Table 'Credits' created successfully.")
+            print("Таблица 'Credits' успешно создана.")
 
             cursor.execute("""
                 CREATE TABLE IF NOT EXISTS Deposits (
-                    deposit_id INTEGER PRIMARY KEY,
+                    deposit_id PRIMARY KEY,
                     client_id INTEGER,
                     amount NUMERIC,
                     duration INTEGER
                 );
             """)
-            print("Table 'Deposits' created successfully.")
+            print("Таблица 'Deposits' успешно создана.")
 
             cursor.close()
+            connection.commit()
             connection.close()
-        except psycopg2.Error as e:
-            error_message = f"{datetime.datetime.now()} - Error while creating tables: {e}"
+        except psycopg2.ProgrammingError as e:
+            error_message = f"{datetime.datetime.now()} - Ошибка при создании таблиц: {e}"
             with open("errors_DB.yml", "a") as error_file:
                 error_file.write(error_message + "\n")
-    if __name__ == "__main__":
-        check_and_create_database()
+
+if __name__ == "__main__":
+    check_and_create_database()
 
